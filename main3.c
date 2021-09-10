dbbcd80280d109120896633407164b7c1b86fb56
AMG215 <abdelrahman2159@gmail.com>
2021-09-10 10:48:07 +0200

    First commit

---
+++ main3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+void swapfn(int,int);
+int main()
+{
+    int z,x,y;
+    printf("Enter the number:\n");
+    scanf("%d\n%d",&x,&y);
+    printf("Number before the swap:\n%d\n%d",x,y);
+    swapfn(x,y);
+    return 0;
+}
+void swapfn(int x,int y)
+{
+   x=x+y;
+   y=x-y;
+   x=x-y;
+   printf("\nNumbers after the swap:\n%d\n%d",x,y);
+
+
+}
